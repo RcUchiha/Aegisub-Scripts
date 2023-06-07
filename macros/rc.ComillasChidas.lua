3659ac083235ae9ce772c0b10c5d581c67d0962f
CiferrC <rc.uchiha@gmail.com>
2023-06-07 14:12:59 -0600

    Actualización a "rc.ComillasChidas.lua"
    - Se modificó el código para evitar que la automatización hiciera cambios en los comentarios.
    
     - Se rectificó la lógica para hacer que el reemplazo de las comillas se efectúe en casos donde su apertura y cierre son en distintas líneas. 
    
    - Se añadió soporte para cambios a más de un conjunto de comillas en una línea de texto.

--- macros/rc.ComillasChidas.lua
+++ macros/rc.ComillasChidas.lua
@@ -3,25 +3,21 @@ script_description = "Reemplaza las comillas simples en el texto de las líneas 
 script_author = "CiferrC"
 script_version = "1.1"
 
--- Variable global para rastrear si la siguiente comilla debe ser de apertura o cierre
-esComillaApertura = true
-
 -- Función para cambiar las comillas en una línea
 function cambiarComillas(subtitles, selected_lines, active_line)
     for _, index in ipairs(selected_lines) do
         local linea = subtitles[index]
 
-        -- Solo reemplaza las comillas fuera de los corchetes {}
+        -- Primero reemplaza las comillas dentro de los corchetes {} para que no sean alteradas
         local nuevaLinea = linea.text:gsub("({[^}]*})", function(match) return match:gsub('"', '\1') end)
-        nuevaLinea = nuevaLinea:gsub('"', function()
-            if esComillaApertura then
-                esComillaApertura = false
-                return '“'
-            else
-                esComillaApertura = true
-                return '”'
-            end
-        end)
+
+        -- Reemplaza las comillas al inicio de una palabra con comillas de apertura
+        nuevaLinea = nuevaLinea:gsub('"%w', function(match) return '“' .. match:sub(2) end)
+
+        -- Reemplaza las comillas al final de una palabra con comillas de cierre
+        nuevaLinea = nuevaLinea:gsub('%w"', function(match) return match:sub(1, -2) .. '”' end)
+
+        -- Restaura las comillas dentro de los corchetes {} a su estado original
         nuevaLinea = nuevaLinea:gsub('\1', '"')
 
         linea.text = nuevaLinea
