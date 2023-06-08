4f3358a5de3dc2fb58e97d726a9591e0ea491248
CiferrC <rc.uchiha@gmail.com>
2023-06-08 02:35:56 -0600

    Actualización a "rc.ComillasChidas.lua"
    - Se corrigió un error que evitaba reemplazar las comillas en las líneas donde hubiera comentarios con comillas por fuera de los signos de puntuación.

--- macros/rc.ComillasChidas.lua
+++ macros/rc.ComillasChidas.lua
@@ -1,24 +1,25 @@
 script_name = "Comillas Chidas"
 script_description = "Reemplaza las comillas simples en el texto de las líneas seleccionadas por comillas de apertura (“) y cierre (”)"
 script_author = "CiferrC"
-script_version = "1.1"
+script_version = "1.2"
 
 -- Función para cambiar las comillas en una línea
 function cambiarComillas(subtitles, selected_lines, active_line)
     for _, index in ipairs(selected_lines) do
         local linea = subtitles[index]
 
-        -- Primero reemplaza las comillas dentro de los corchetes {} para que no sean alteradas
-        local nuevaLinea = linea.text:gsub("({[^}]*})", function(match) return match:gsub('"', '\1') end)
+        -- Extraer y almacenar los comentarios para evitar verificarlos
+        local comentarios = {}
+        local nuevaLinea = linea.text:gsub("({[^}]*})", function(match) 
+            table.insert(comentarios, match)
+            return "{}"
+        end)
 
         -- Reemplaza las comillas al inicio de una palabra con comillas de apertura
         nuevaLinea = nuevaLinea:gsub('"%w', function(match) return '“' .. match:sub(2) end)
 
         -- Reemplaza las comillas al final de una palabra con comillas de cierre
         nuevaLinea = nuevaLinea:gsub('%w"', function(match) return match:sub(1, -2) .. '”' end)
-
-        -- Restaura las comillas dentro de los corchetes {} a su estado original
-        nuevaLinea = nuevaLinea:gsub('\1', '"')
 
         -- Verifica si hay comillas antes de los signos de apertura ¡ y ¿, o después de los signos de cierre ! y ?
         if nuevaLinea:find('"%¡') or nuevaLinea:find('"%¿') or nuevaLinea:find('!%"') or nuevaLinea:find('?%"') then
@@ -33,6 +34,13 @@ function cambiarComillas(subtitles, selected_lines, active_line)
             linea.effect = "Comillas por fuera de punto"
             nuevaLinea = linea.text
         end
+
+        -- Inserta los comentarios de nuevo en su lugar original
+        local i = 0
+        nuevaLinea = nuevaLinea:gsub("({})", function() 
+            i = i + 1
+            return comentarios[i]
+        end)
 
         linea.text = nuevaLinea
         subtitles[index] = linea
