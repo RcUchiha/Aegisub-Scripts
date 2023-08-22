edacb6f72391e8550965cdd7d04d435b488e2228
CiferrC <rc.uchiha@gmail.com>
2023-08-21 23:49:00 -0500

    Actualización a "rc.ComillasChidas.lua"
    - Se añadió soporte para que la macro admita otros caracteres no alfanuméricos (ôōûū~) al hacer reemplazos de comillas que los rodeen.
    
    - Se añadieron leyendas independientes sugerentes para el campo de "efecto" en las líneas con casos de comillas fuera de lugar en signos de exclamación, interrogación y punto final, así como notas informativas comentadas en las mismas.

--- macros/rc.ComillasChidas.lua
+++ macros/rc.ComillasChidas.lua
@@ -1,7 +1,7 @@
 script_name = "Comillas Chidas"
 script_description = "Reemplaza las comillas simples en el texto de las líneas seleccionadas por comillas latinas de apertura (“) y cierre (”)"
 script_author = "CiferrC"
-script_version = "1.3"
+script_version = "1.4"
 
 menu_embedding = "CiferrC/"
 
@@ -17,34 +17,42 @@ function cambiarComillas(subtitles, selected_lines, active_line)
             return "{}"
         end)
 
-        -- Reemplaza las comillas al inicio de una palabra con comillas de apertura
-        -- Incluye palabras que comienzan con letras acentuadas
-        nuevaLinea = nuevaLinea:gsub('"[%wÁÉÍÓÚáéíóú]', function(match) return '“' .. match:sub(2) end)
+        -- Reemplaza las comillas simples al inicio de una palabra con comillas latinas de apertura
+        -- Incluye palabras que comienzan con letras acentuadas y otros caracteres
+        nuevaLinea = nuevaLinea:gsub('"[%wÁÉÍÓÚáéíóúôōûū~]', function(match) return '“' .. match:sub(2) end)
 
-        -- Reemplaza las comillas al final de una palabra con comillas de cierre
-        -- Incluye palabras que terminan con letras acentuadas
-        nuevaLinea = nuevaLinea:gsub('[%wÁÉÍÓÚáéíóú]"', function(match) return match:sub(1, -2) .. '”' end)
+        -- Reemplaza las comillas simples al final de una palabra con comillas latinas de cierre
+        -- Incluye palabras que terminan con letras acentuadas y otros caracteres
+        nuevaLinea = nuevaLinea:gsub('[%wÁÉÍÓÚáéíóúôōûū~]"', function(match) return match:sub(1, -2) .. '”' end)
 
-        -- Verifica si hay comillas antes de los signos de apertura ¡ y ¿, o después de los signos de cierre ! y ?
-        if nuevaLinea:find('"%¡') or nuevaLinea:find('"%¿') or nuevaLinea:find('!%"') or nuevaLinea:find('?%"') then
-            -- Si hay, agrega el aviso al campo "Efecto" precedido por una barra diagonal (/) y espacios si ya hay un comentario
+        -- Verifica si hay comillas por fuera de signos de exclamación
+        if nuevaLinea:find('"%¡') or nuevaLinea:find('!%"') then
             if linea.effect and #linea.effect > 0 then
-                linea.effect = linea.effect .. " / Comillas por fuera de signos"
+                linea.effect = linea.effect .. " / Comillas por fuera de exclamación"
             else
-                linea.effect = "Comillas por fuera de signos"
+                linea.effect = "Comillas por fuera de exclamación"
             end
-            nuevaLinea = linea.text
+            nuevaLinea = nuevaLinea .. " {NOTA: Los signos de exclamación se ponen dentro de las comillas (“¡ !”) solo si se trata de una cita exclamativa.}"
+        end
+
+        -- Verifica si hay comillas por fuera de signos de interrogación
+        if nuevaLinea:find('"%¿') or nuevaLinea:find('?%"') then
+            if linea.effect and #linea.effect > 0 then
+                linea.effect = linea.effect .. " / Comillas por fuera de interrogación"
+            else
+                linea.effect = "Comillas por fuera de interrogación"
+            end
+            nuevaLinea = nuevaLinea .. " {NOTA: Los signos de interrogación se ponen dentro de las comillas (“¿ ?”) solo si se trata de una cita interrogativa.}"
         end
 
         -- Verifica si hay comillas después del punto
         if nuevaLinea:find('%."') then
-            -- Si hay, agrega un aviso diferente al campo "Efecto" precedido por una barra diagonal (/) y espacios si ya hay un comentario
             if linea.effect and #linea.effect > 0 then
                 linea.effect = linea.effect .. " / Comillas por fuera de punto"
             else
                 linea.effect = "Comillas por fuera de punto"
             end
-            nuevaLinea = linea.text
+            nuevaLinea = linea.text .. " {NOTA: El punto final bajo ninguna circunstancia va dentro de las comillas (“___.”). [https://www.fundeu.es/consulta/las-comillas-y-el-punto-final-6553/]}"
         end
 
         -- Inserta los comentarios de nuevo en su lugar original
